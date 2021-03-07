dbe63da248d4603ce1d13efdb80160e26d1eadd0
Felipe Rodrigues <felipe@crafters.cc>
2021-03-07 18:48:51 -0300

    health_check integrations tests

---
+++ tests/health_check_test.rs
@@ -0,0 +1,41 @@
+use std::fs::File;
+use std::io::Read;
+use std::net::{TcpListener, SocketAddr};
+use reqwest::Client;
+
+#[actix_rt::test]
+async fn health_check_works() {
+    let (client, address) = setup();
+
+    let response = client
+        .get(&format!("{}/health_check", address))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn setup() -> (Client, String) {
+    let address = spawn_app();
+
+    let mut buf = Vec::new();
+    let _ = File::open("cert.pem")
+        .unwrap()
+        .read_to_end(&mut buf);
+
+    let client = Client::builder()
+        .add_root_certificate(reqwest::Certificate::from_pem(&buf).unwrap())
+        .build()
+        .unwrap();
+    (client, address)
+}
+
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let address = listener.local_addr().unwrap();
+    let server = tattico::run(listener).expect("Failed to bind address.");
+    let _ = tokio::spawn(server);
+    format!("https://{}", address)
+}
