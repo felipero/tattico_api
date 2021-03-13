594d441bb9d18c5c05e7169a839f81efd62506b7
Felipe Rodrigues <felipe@crafters.cc>
2021-03-13 20:55:52 -0300

    Adding ssl certificates to github actions: test 1

--- src/lib.rs
+++ src/lib.rs
@@ -47,8 +47,8 @@ fn logger_setup() {
 fn ssl_builder() -> SslAcceptorBuilder {
     let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
     builder
-        .set_private_key_file("key.pem", SslFiletype::PEM)
+        .set_private_key_file("ssl/key.pem", SslFiletype::PEM)
         .unwrap();
-    builder.set_certificate_chain_file("cert.pem").unwrap();
+    builder.set_certificate_chain_file("ssl/cert.pem").unwrap();
     builder
 }
